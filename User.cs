cf7544d3f8d730b75da5764f876c2952576c60d3
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-25 14:03:17 +0200

    add CheckCourses method, courseList as class var
    To keep track of each users registered courses and generate that list

--- User.cs
+++ User.cs
@@ -9,6 +9,8 @@ abstract class User
     public string Username;
     protected string _password;
     public string Email;
+    List<string> courseList = new List<string>();
+
     public User(string name, string username, string email)
     {
         Name = name;
@@ -23,13 +25,29 @@ abstract class User
                  $"Email:        {Email}\n";
         if (inclPassword)
         {
-        txt +=   $"Lösenord:     {_password}";
+            txt += $"Lösenord:     {_password}";
         }
         return txt;
     }
     public void SetPassword(string password)
     {
         _password = password;
-        Utility.Success("Lösenord skapades!",false);
+        Utility.Success("Lösenord skapades!", false);
+    }
+    public void CheckCourses(List<string> CheckCourseList)
+    {
+        void UpdateCourses(List<string> UpdateCourseList)
+        {
+            courseList.Clear();
+            foreach (string course in UpdateCourseList)
+            {
+                courseList.Add(course);
+            }
+        }
+        if (CheckCourseList.Count != 0)
+        {
+            UpdateCourses(CheckCourseList);
+            Utility.GenerateMenu(title: "Kurser", choices: courseList.ToArray());
+        }
     }
 }
