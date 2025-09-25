50b9aaee4a567be83b800ea0d116b1c1da202777
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-25 13:32:48 +0200

    Add Systemclass with checkCourses method
    To store dictionaries, lists and methods for the main program

--- SystemClass.cs
+++ SystemClass.cs
@@ -2,5 +2,26 @@ namespace App;
 
 class SystemClass
 {
+    public List<User> users = new List<User>();
+    public Dictionary<string, List<User>> attendeesByCourse = new Dictionary<string, List<User>>();
 
+    public List<string> CheckCourses(User user)
+    {
+        List<string> courseList = new List<string>();
+        foreach ((string course, List<User> users1) in attendeesByCourse)
+        {
+            foreach (User user1 in users1)
+            {
+                if (user == user1)
+                {
+                    courseList.Add(course);
+                }
+            }
+        }
+        if (courseList.Count == 0)
+        {
+            Utility.Error("Du är inte registrerad på några kurser");
+        }
+        return courseList;
+    }
 }
