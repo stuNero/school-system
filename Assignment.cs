0f1fc86782e6219e247e93700d6eb1c6da9e906a
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-25 14:19:00 +0200

    add Assignment class with AddContent method
    to store assignment info

---
+++ Assignment.cs
@@ -0,0 +1,18 @@
+using App;
+
+class Assignment
+{
+    public string Name;
+    public string Id;
+    public string Content;
+
+    public Assignment(string name, string id)
+    {
+        Name = name;
+        Id = id;
+    }
+    public void addContent(string text)
+    {
+        this.Content += text;
+    }
+}
