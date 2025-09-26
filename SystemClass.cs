d46ac288c70e60e695b3cc5a79ce84a924cca064
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-26 08:23:28 +0200

    add ExportUserProfile method
    to save users between runtimes

--- SystemClass.cs
+++ SystemClass.cs
@@ -5,6 +5,14 @@ class SystemClass
     public List<User> users = new List<User>();
     public List<Course> courses = new List<Course>();
 
+    // Directories to saved data
+    string SavedUsersDir = "saved_users.md";
+    string SavedCoursesDir = "saved_users.md";
+
+    public SystemClass()
+    {
+        // List<string> usersFromFile = ImportUserProfile(SavedUsersDir);
+    }
     public List<Course> GatherCourses(User user)
     {
         List<Course> coursesByUser = new List<Course>();
@@ -24,5 +32,65 @@ class SystemClass
         {
             return coursesByUser;
         }
+    }
+    // public static List<string> ImportUserProfile(string fileName)
+    // {
+    //     List<string> fileContent = new List<string>();
+    //     using (StreamReader reader = new StreamReader(fileName))
+    //     {
+    //         string line;
+    //         string start = "--new_user--";
+    //         string end = "--end_user--";
+    //         bool insideBlock = false;
+    //         while ((line = reader.ReadLine()) != null)
+    //         {
+    //             if (line.Trim() == start)
+    //             {
+    //                 insideBlock = true;
+    //             }
+    //             if(line.Trim() == end)
+    //             {
+    //                 insideBlock = false;
+    //             }
+    //             if (insideBlock)
+    //             {
+    //                 if (!fileContent.Contains(line))
+    //                 {
+    //                     fileContent.Add(line);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return fileContent;
+    // }
+    public void ExportUserProfile(User user = null)
+    {
+        if (user == null)
+        {
+            using (StreamWriter writer = new StreamWriter(SavedUsersDir))
+            {
+                foreach (User user1 in this.users)
+                {
+                    writer.WriteLine("");
+                    writer.WriteLine("--new_user--");
+                    writer.WriteLine(user1.Info(inclPassword: true));
+                    writer.WriteLine("--end_user--");
+                }
+            }
+        }
+        else
+        {
+            using (StreamWriter writer = new StreamWriter(SavedUsersDir, append: true))
+            {
+                writer.WriteLine("");
+                writer.WriteLine("--new_user--");
+                writer.WriteLine(user.Info(inclPassword: true));
+                writer.WriteLine("--end_user--");
+            }
+        }
+    }
+    public void ExportCourse(Course course)
+    {
+
     }
 }
